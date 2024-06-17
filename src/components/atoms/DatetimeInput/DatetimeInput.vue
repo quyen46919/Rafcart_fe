ccf69a8480126978a646b01c68c19f96bb0ec6d9
NghiemLeDo <nghiemledo@gmail.com>
2024-06-17 21:18:23 +0700

    feat: init project

---
+++ src/components/atoms/DatetimeInput/DatetimeInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- datetimeinput -->
+  <div class="">
+    <input type="text" id="datetime-input"
+      class="h-[42px] w-[259px] text-gray-600 uppercase text-sm rounded border-gray-300" placeholder="MM/YY"
+      v-model="date" @input="formatDate" maxlength="5" />
+  </div>
+  <!-- datetimeinput end -->
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const date = ref('');
+const formatDate = () => {
+  let formattedDate = date.value.replace(/[^\d]/g, '').slice(0, 4);
+  if (formattedDate.length === 4) {
+    formattedDate = formattedDate.slice(0, 2) + '/' + formattedDate.slice(2);
+  }
+  date.value = formattedDate;
+};
+</script>
