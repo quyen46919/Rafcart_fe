206dfa7039f3ac3049c7defcd0dad870f4a08627
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-06-17 17:13:18 +0700

    feat: create-checkbox

---
+++ src/components/atoms/Checkbox/CheckBox.stories.ts
@@ -0,0 +1,55 @@
+import { Checkout } from '@/pages/checkout/Checkout.vue'
+import { Meta, StoryObj } from '@storybook/vue3'
+import CheckBox from './CheckBox.vue'
+import { h } from 'vue'
+
+const meta: Meta<typeof CheckBox> = {
+  component: CheckBox,
+  title: 'Components/CheckBox',
+  argTypes: {
+    label: {
+      control: 'text'
+    },
+    checked: {
+      control: 'boolean'
+    },
+    isDisabled: {
+      control: 'boolean'
+    },
+    readonly: {
+      control: 'boolean'
+    }
+  }
+}
+export default meta
+
+type Story = StoryObj<typeof CheckBox>
+
+export const Primary: Story = {
+  render: (args: any) => ({
+    components: { CheckBox },
+    setup() {
+      const handleOnClick = (event: MouseEvent) => {
+        const target = event.target as HTMLInputElement
+        console.log(target.value)
+      }
+      return { args, handleOnClick }
+    },
+    template: '<CheckBox v-bind="args" :onClick="handleOnClick"/> '
+  }),
+  tags: ['autodocs'],
+  args: {
+    checked: false,
+
+    isDisabled: false,
+    readonly: false,
+    label: h('span', {}, [
+      'I have read and agree to the ',
+      h(
+        'a',
+        { href: 'https://storybook.js.org/docs/writing-docs', target: 'blank', class: 'custom-link text-red-500' },
+        'terms & conditions'
+      )
+    ])
+  }
+}
---
+++ src/components/atoms/Checkbox/CheckBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex items-center">
+    <input
+      id="checkbox"
+      :class="{
+        'border-gray-300 text-gray-300 mr-3 ring-none rounded-sm': props?.isDisabled == true,
+        'border-red-500 text-red-500 mr-3 ring-none rounded-sm': props?.isDisabled == false
+      }"
+      type="checkbox"
+      :disabled="props?.isDisabled || props?.readonly"
+      @click="props.onClick"
+      :checked="props.checked"
+      style="box-shadow: none"
+      :style="{ cursor: props?.isDisabled || props?.readonly ? 'default' : 'pointer' }"
+    />
+    <label
+      for="checkbox"
+      class="flex items-center w-fit"
+      :style="{ cursor: props?.isDisabled || props?.readonly ? 'default' : 'pointer' }"
+    >
+      <span v-if="typeof props.label == 'string'">{{ props.label }}</span>
+      <component :is="props.label" v-else />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, withDefaults, VNode } from 'vue'
+
+interface InputProps {
+  checked: boolean
+  label?: string | VNode
+  isDisabled?: boolean
+  readonly?: boolean
+  onClick: (event: MouseEvent) => void
+}
+
+const props = withDefaults(defineProps<InputProps>(), {
+  checked: false,
+  isDisabled: false,
+  readonly: false
+})
+</script>
