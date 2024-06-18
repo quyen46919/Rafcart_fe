fb9620a2428c24e8880ed01660ea263bd6b9d510
TranDucTuan2001 <tuan95373@donga.edu.vn>
2024-06-18 16:51:37 +0700

    fix-review button component

--- src/components/atoms/RButton/RButton.stories.ts
+++ src/components/atoms/RButton/RButton.stories.ts
@@ -1,80 +1,69 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
-import RButton from './RButton.vue';
+import type { Meta, StoryObj } from '@storybook/vue3'
+import RButton from './RButton.vue'
 
 const meta: Meta<typeof RButton> = {
   component: RButton,
   argTypes: {
     variant: {
       control: { type: 'select' },
-      options: ['text', 'contained', 'outlined', 'icon'],
+      options: ['text', 'contained', 'outlined', 'icon']
     },
     content: { control: 'text' },
     prefixIcon: { control: 'text' },
     suffixIcon: { control: 'text' },
     circle: { control: 'boolean' },
-    onClick: { action: 'clicked' },
+    onClick: { action: 'click' },
     isDisabled: { control: 'boolean' },
-    isLoading: { control: 'boolean' },
-    color: { control: 'color' }, // Thêm thuộc tính color
-  },
-};
+    isLoading: { control: 'boolean' }
+  }
+}
 
-export default meta;
-type Story = StoryObj<typeof RButton>;
+export default meta
+type Story = StoryObj<typeof RButton>
 
 export const Contained: Story = {
   args: {
     variant: 'contained',
     content: 'Add to Cart',
-    prefixIcon: 'fas fa-shopping-cart',
-    onClick: () => { console.log('Add to Cart clicked') },
-    color: '#f44336', // Màu tùy chỉnh
-  },
-};
+    prefixIcon: 'fas fa-shopping-cart'
+  }
+}
 
 export const Outlined: Story = {
   args: {
     variant: 'outlined',
     content: 'Wishlist',
-    prefixIcon: 'fas fa-heart',
-    onClick: () => { console.log('Wishlist clicked') },
-    color: '#e01750', // Màu tùy chỉnh
-  },
-};
+    prefixIcon: 'fas fa-heart'
+  }
+}
 
 export const Text: Story = {
   args: {
     variant: 'text',
-    content: 'Facebook',
-    onClick: () => { console.log('Facebook clicked') },
-    color: '#4267B2', // Màu tùy chỉnh
-  },
-};
+    content: 'Facebook'
+  }
+}
 
 export const Icon: Story = {
   args: {
     variant: 'icon',
-    prefixIcon: 'fa-brands fa-facebook-f',
-    onClick: () => { console.log('Icon clicked') },
-    color: '#4267B2', // Màu tùy chỉnh
-  },
-};
+    prefixIcon: 'fa-brands fa-facebook-f'
+  }
+}
 
 export const Circle: Story = {
   args: {
-    variant: 'icon',
+    variant: 'outlined',
     prefixIcon: 'fas fa-th',
-    circle: true,
-    onClick: () => { console.log('Circle icon clicked') },
-    color: '#ffc107', // Màu tùy chỉnh
-  },
-};
+    content: 'Setting',
+    circle: true
+  }
+}
 
 export const Loading: Story = {
   args: {
     variant: 'contained',
     content: 'Loading',
-    isLoading: true,
-    color: '#28a745', // Màu tùy chỉnh
-  },
-};
+    isLoading: true
+  }
+}
