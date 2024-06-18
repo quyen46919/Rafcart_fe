5be24044618d657bce31d0812bab971c39473bf1
TranDucTuan2001 <tuan95373@donga.edu.vn>
2024-06-18 22:12:26 +0700

    create IntoCategoryBox

---
+++ src/components/molecules/IntroCategoryBox/IntroCategoryBox.stories.ts
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/vue3';
+import IntroCategoryBox from './IntroCategoryBox.vue';
+import CategoryProps from '../../../interfaces/category'
+import Bedroom from '@/assets/images/category/category-1.jpg'
+const meta: Meta<typeof IntroCategoryBox> = {
+  component: IntroCategoryBox,
+  argTypes: {
+    id: { control: 'number' },
+    name: { control: 'text' },
+    thumbnail_url: { control: 'text' },
+    slug: { control: 'text' }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof IntroCategoryBox>;
+
+export const Primary: Story = {
+  render: (args: CategoryProps) => ({
+    components: { IntroCategoryBox },
+    setup() {
+      return { args };
+    },
+    template: '<IntroCategoryBox v-bind="args" />'
+  }),
+  args: {
+    id: 1,
+    name: 'Bedroom',
+    thumbnail_url: Bedroom,
+    slug: 'bedroom'
+  }
+};
