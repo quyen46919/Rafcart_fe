b46e18c63292121e03c3a0360c208d5b022f3831
TranDucTuan2001 <tuan95373@donga.edu.vn>
2024-06-18 16:59:16 +0700

    Return card.stories to original

--- src/components/atoms/Loader/Loader.vue
+++ src/components/atoms/Loader/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div role="status">
+  <div role="status" class="flex items-center justify-center">
     <svg
       aria-hidden="true"
       class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
