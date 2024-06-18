4e394fb72be4b80a4b89055979e1bb6a1e72608c
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-06-18 15:12:50 +0700

    feat: creat-serviceCard

---
+++ src/components/molecules/ServiceCard/ServiceCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="flex justify-center w-full bg-white lg:max-w-[310px] py-9 px-20 lg:py-7 lg:px-16 border border-primary rounded-sm"
+  >
+    <img class="w-[50px] h-[38px]" :src="props.image" alt="image" />
+    <div class="flex flex-col gap-y-1 ml-4 justify-between">
+      <span class="text-sm font-semibold line-clamp-1">{{ title }} </span>
+      <span class="text-xs text-[#737679] line-clamp-1">{{ description }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+interface ServiceCardProps {
+  image?: string
+  title?: string
+  description?: string
+}
+
+const props = withDefaults(defineProps<ServiceCardProps>(), {
+  image: '',
+  title: '',
+  description: ''
+})
+</script>
