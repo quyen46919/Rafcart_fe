fe6b77f7f0c3798d54962564f8a7cd6deb02a6e4
luan-thnh <luanthanh0322@gmail.com>
2024-06-19 16:29:51 +0700

    feat: create rating

--- src/components/atoms/Rating/Rating.stories.ts
+++ src/components/atoms/Rating/Rating.stories.ts
@@ -3,13 +3,49 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import Rating from './Rating.vue'
 
 const meta: Meta<typeof Rating> = {
-  component: Rating
+  component: Rating,
+  tags: ['autodocs'],
+  argTypes: {
+    status: { control: 'select', options: ['text', 'number'] }
+  }
 }
 
 export default meta
 type Story = StoryObj<typeof Rating>
 
-export const Primary: Story = {
+export const HiddenRating: Story = {
+  render: (args: any) => ({
+    components: { Rating },
+    setup() {
+      return { args }
+    },
+    template: '<Rating v-bind="args" />'
+  }),
+  args: {
+    rating: 8,
+    maxRating: 10,
+    showRating: false,
+    readOnly: true
+  }
+}
+
+export const ReviewsRating: Story = {
+  render: (args: any) => ({
+    components: { Rating },
+    setup() {
+      return { args }
+    },
+    template: '<Rating v-bind="args" />'
+  }),
+  args: {
+    rating: 7,
+    maxRating: 10,
+    showRating: true,
+    note: '50 Reviews'
+  }
+}
+
+export const StatusRating: Story = {
   render: (args: any) => ({
     components: { Rating },
     setup() {
@@ -19,6 +55,8 @@ export const Primary: Story = {
   }),
   args: {
     rating: 6,
-    maxRating: 10
+    maxRating: 10,
+    showRating: true,
+    status: 'text'
   }
 }
--- src/components/atoms/Rating/Rating.vue
+++ src/components/atoms/Rating/Rating.vue
@@ -1,24 +1,80 @@
 <template>
-  <div class="flex gap-1 text-sm text-yellow-400">
-    <span v-for="(_, index) in props.maxRating" :key="index">
-      <i
-        class="fas fa-star"
-        :class="{ 'text-yellow-400': index < props.rating, 'text-gray-300': index >= props.rating }"
-      ></i>
-    </span>
+  <div class="flex items-center gap-3">
+    <div @mouseleave="mouseOverRating = null">
+      <star-rating
+        :show-rating="false"
+        :rating="props?.rating"
+        :star-size="props?.size"
+        :read-only="props?.readOnly"
+        :max-rating="props?.maxRating"
+        @hover:rating="handleMouseOverRating"
+        v-model:rating="clickRatingText"
+      ></star-rating>
+    </div>
+
+    <p class="text-gray-500 font-medium text-lg" v-if="props?.showRating">
+      <span
+        v-if="props?.status === 'number'"
+        class="max-w-[150px] text-ellipsis overflow-hidden whitespace-nowrap block"
+        >{{ note }}</span
+      >
+      <span v-else>{{ mouseOverRating ? ratingStatus : ratingClickStatus }}</span>
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, onMounted } from 'vue'
+import StarRating from 'vue-star-rating'
+
 interface RatingProps {
+  size?: number
   rating: number
   maxRating: number
+  readOnly?: boolean
+  showRating?: boolean
+  note?: string
+  status?: 'text' | 'number'
 }
-const props = withDefaults(
-  defineProps<RatingProps>(),
-  {
-    rating: 0,
-    maxRating: 5
+const props = withDefaults(defineProps<RatingProps>(), {
+  size: 24,
+  rating: 0,
+  maxRating: 5,
+  showRating: false,
+  status: 'number'
+})
+
+const rating = ref(null)
+const mouseOverRating = ref(null)
+const clickRatingText = ref(props?.rating)
+
+const handleMouseOverRating = (e) => {
+  mouseOverRating.value = e
+}
+
+const ratingStatus = computed(() => {
+  return mouseOverRating.value !== null ? getRatingStatus(mouseOverRating.value) : ''
+})
+
+const ratingClickStatus = computed(() => {
+  return getRatingStatus(clickRatingText.value)
+})
+
+const formatNumber = (number) => (number < 10 ? '0' + number : number)
+
+const getRatingStatus = (rating: number) => {
+  const ratio = rating / props.maxRating
+  switch (true) {
+    case ratio >= 0.8:
+      return 'Excellent'
+    case ratio >= 0.6:
+      return 'Very good'
+    case ratio >= 0.4:
+      return 'Good'
+    case ratio >= 0.2:
+      return 'Average'
+    default:
+      return 'Poor'
   }
-)
+}
 </script>
