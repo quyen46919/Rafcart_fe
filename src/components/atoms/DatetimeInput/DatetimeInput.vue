338523434b45f27343377afb18d658115692c712
NghiemLeDo <nghiemledo@gmail.com>
2024-06-19 22:28:37 +0700

    fix: UI and formatDatetime

--- src/components/atoms/DatetimeInput/DatetimeInput.vue
+++ src/components/atoms/DatetimeInput/DatetimeInput.vue
@@ -1,22 +1,21 @@
 <template>
   <!-- datetimeinput -->
-  <div class="">
-    <input type="text" id="datetime-input"
-      class="h-[42px] w-[259px] text-gray-600 uppercase text-sm rounded border-gray-300" placeholder="MM/YY"
-      v-model="date" @input="formatDate" maxlength="5" />
+  <div class="flex justify-between w-[259px]">
+    <label for="datetime-input" class="block text-gray-700 text-sm font-[500] mb-2">{{ labelName }}</label>
+    <span v-if="isRequired" class="text-red-400">&#42;</span>
   </div>
+  <input type="date" class="h-[42px] w-[259px] text-gray-600 uppercase text-sm rounded border-gray-300"
+    placeholder="MM/YY" v-model="date" @input="formatDate" maxlength="10" />
   <!-- datetimeinput end -->
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, defineProps } from 'vue';
+import * as moment from 'moment';
+import { DateFormat } from '@/shared/enum/format';
+
+const props = defineProps<{ labelName: string; isRequired: boolean }>()
 
 const date = ref('');
-const formatDate = () => {
-  let formattedDate = date.value.replace(/[^\d]/g, '').slice(0, 4);
-  if (formattedDate.length === 4) {
-    formattedDate = formattedDate.slice(0, 2) + '/' + formattedDate.slice(2);
-  }
-  date.value = formattedDate;
-};
+const formatDate = () => moment(date.value).format(DateFormat.YYYYMM)
 </script>
