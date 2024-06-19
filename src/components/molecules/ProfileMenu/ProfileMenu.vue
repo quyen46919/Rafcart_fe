806b3fd40f2f6628a78c2582b449b79cce3eaad8
VoxTus <votu10032003@gmail.com>
2024-06-19 15:03:48 +0700

    feat: create ProfileMenu

--- src/components/molecules/ProfileMenu/ProfileMenu.vue
+++ src/components/molecules/ProfileMenu/ProfileMenu.vue
@@ -2,22 +2,22 @@
   <!-- sidebar -->
   <div class="col-span-3">
     <!-- account profile -->
-    <div class="px-4 py-3 shadow flex items-center gap-4">
+    <div class="px-4 py-3 w-[280px] h-[66px] shadow flex items-center gap-4">
       <div class="flex-shrink-0">
         <img :src="user?.avatar" class="rounded-full w-14 h-14 p-1 border border-gray-200 object-cover" />
       </div>
       <div>
-        <p class="text-gray-600">Hello,</p>
-        <h4 class="text-gray-800 capitalize font-medium">{{ user?.username }}</h4>
+        <p class="text-gray-600 text-base ">Hello, </p>
+        <h4 class="text-gray-800 text-lg capitalize font-medium">{{ user?.username }}</h4>
       </div>
     </div>
     <!-- account profile end -->
 
     <!-- profile links -->
-    <div class="mt-6 bg-white shadow rounded p-4 divide-y divide-gray-200 space-y-4 text-gray-600">
+    <div class="mt-6 bg-white shadow rounded w-[280px] p-4 divide-y divide-gray-200 space-y-4 text-gray-600">
       <!-- single link -->
       <div class="space-y-1 pl-8">
-        <a href="account.html" class="relative text-base font-medium capitalize hover:text-primary transition block">
+        <a href="account.html" class="relative  text-gray-800  font-medium capitalize hover:text-primary transition block">
           Manage account
           <span class="absolute -left-8 top-0 text-base">
             <i class="far fa-address-card"></i>
@@ -56,7 +56,7 @@
       <div class="pl-8 pt-4">
         <a
           href="wishlist.html"
-          class="relative medium capitalize font-medium hover:text-primary transition block text-primary"
+          class="relative medium text-gray-800 capitalize font-medium hover:text-primary transition block "
         >
           my wishlist
           <span class="absolute -left-8 top-0 text-base">
@@ -67,7 +67,7 @@
       <!-- single link end -->
       <!-- single link -->
       <div class="pl-8 pt-4">
-        <a href="#" class="relative medium capitalize text-gray-800 font-medium hover:text-primary transition block">
+        <a href="#" class="relative medium  capitalize text-gray-800 font-medium hover:text-primary transition block">
           logout
           <span class="absolute -left-8 top-0 text-base">
             <i class="fas fa-sign-out-alt"></i>
