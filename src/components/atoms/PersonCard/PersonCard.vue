30df8fb7c364329d423797dbe4c48d4ddf28c786
VoxTus <votu10032003@gmail.com>
2024-06-19 09:59:09 +0700

    feat: update  PersonCard

--- src/components/atoms/PersonCard/PersonCard.vue
+++ src/components/atoms/PersonCard/PersonCard.vue
@@ -1,9 +1,9 @@
  <template>
   <div class="w-full max-w-60 bg-white  rounded-lg flex">
   <a href="#">
-        <img class="rounded-t-lg " src="/src/assets/images/avtcard.jpg" alt="product image" />
-         <h3 class="pt-3 text-xl text-center font-medium text-gray-900 ">Jane Copper</h3>
-        <p class="pt-0 text-lg text-center text-gray-600 ">Founder</p>
+        <img class="rounded-t-lg w-[275px] h-[285px]  " src="/src/assets/images/avtcard.jpg" alt="product image" />
+         <h3 class="pt-3 text-sm text-center font-medium text-gray-900 ">Jane Copper 270 , 285</h3>
+        <p class="pt-0 text-sm text-center text-gray-600 ">Founder</p>
     </a>
   </div>
 </template>
