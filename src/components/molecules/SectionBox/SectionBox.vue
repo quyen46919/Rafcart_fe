427c58188bd51df1b224eb867747baf5dee08b44
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-06-19 15:53:29 +0700

    feat: create-section-box

---
+++ src/components/molecules/SectionBox/SectionBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex text-xl capitalize items-center font-medium h-10 w-full pl-4 bg-[#e9e4e4]">{{ section }}</div>
+</template>
+<script setup lang="ts">
+interface sectionBoxProp {
+  section: string
+}
+const props = withDefaults(defineProps<sectionBoxProp>(), {})
+</script>
