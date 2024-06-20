76f4fee5691cb7f0e1748947a6e7010bf9a7c86d
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-06-20 20:03:53 +0700

    fix: service card

--- src/components/molecules/ServiceCard/ServiceCard.vue
+++ src/components/molecules/ServiceCard/ServiceCard.vue
@@ -3,9 +3,9 @@
     class="flex justify-center w-full bg-white lg:max-w-[310px] py-9 px-20 lg:py-7 lg:px-16 border border-primary rounded-sm"
   >
     <img class="w-[50px] h-[38px]" :src="props.image" alt="image" />
-    <div class="flex flex-col gap-y-1 ml-4 justify-between">
-      <span class="text-sm font-semibold line-clamp-1">{{ title }} </span>
-      <span class="text-xs text-[#737679] line-clamp-1">{{ description }}</span>
+    <div class="flex flex-col gap-y-1 ml-4 justify-between overflow-hidden">
+      <span class="text-sm font-semibold line-clamp-1 truncate">{{ title }}</span>
+      <span class="text-xs text-[#737679] line-clamp-1 truncate">{{ description }}</span>
     </div>
   </div>
 </template>
@@ -14,9 +14,9 @@
 import { defineProps, withDefaults } from 'vue'
 
 interface ServiceCardProps {
-  image?: string
-  title?: string
-  description?: string
+  image: string
+  title: string
+  description: string
 }
 
 const props = withDefaults(defineProps<ServiceCardProps>(), {
