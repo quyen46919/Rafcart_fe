2e6ef2a25eb4f174cc9c789659a494ba0b0ffd29
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-06-21 10:25:47 +0700

    fix: service card 'text-ellipsis'

--- src/components/molecules/ServiceCard/ServiceCard.vue
+++ src/components/molecules/ServiceCard/ServiceCard.vue
@@ -3,9 +3,9 @@
     class="flex justify-center w-full bg-white lg:max-w-[310px] py-9 px-20 lg:py-7 lg:px-16 border border-primary rounded-sm"
   >
     <img class="w-[50px] h-[38px]" :src="props.image" alt="image" />
-    <div class="flex flex-col gap-y-1 ml-4 justify-between overflow-hidden">
-      <span class="text-sm font-semibold line-clamp-1 truncate">{{ title }}</span>
-      <span class="text-xs text-[#737679] line-clamp-1 truncate">{{ description }}</span>
+    <div class="ml-4 overflow-hidden text-ellipsis">
+      <p class="text-sm font-semibold truncate">{{ title }}</p>
+      <p class="text-xs text-[#737679] truncate">{{ description }}</p>
     </div>
   </div>
 </template>
