501ec5bc50b5ca0e5d506fa99a8bd6ed08361022
NghiemLeDo <nghiemledo@gmail.com>
2024-06-21 17:51:01 +0700

    fix: format Datetime

--- src/shared/enum/format.ts
+++ src/shared/enum/format.ts
@@ -1,6 +1,6 @@
 export enum DateFormat {
-    YYYYMM = 'YYYYMM',
-    YYYYMMDD = 'YYYYMMDD',
-    MMDDYYYY = 'MMDDYYYY',
-    DDMMYYYY = 'DDMMYYYY'
+    YYYYMM = 'yyyy/MM',
+    YYYYMMDD = 'yyyy/MM/dd',
+    MMDDYYYY = 'MM/dd/yyyy',
+    DDMMYYYY = 'dd/MM/yyyy'
 }
