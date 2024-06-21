7a631a84f3c73aae0b4aa17acfb1693f5717bcab
luan-thnh <luanthanh0322@gmail.com>
2024-06-21 22:29:02 +0700

    feat: create category-menu

---
+++ src/faker/home/brands.ts
@@ -0,0 +1,62 @@
+import Brand from '@/interfaces/brand'
+
+const brands: Brand[] = [
+  {
+    slug: 'brand_1',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-1.png'
+  },
+  {
+    slug: 'brand_2',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-2.png'
+  },
+  {
+    slug: 'brand_3',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-3.png'
+  },
+  {
+    slug: 'brand_4',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-4.png'
+  },
+  {
+    slug: 'brand_5',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-5.png'
+  },
+  {
+    slug: 'brand_6',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-6.png'
+  },
+  {
+    slug: 'brand_7',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-7.png'
+  },
+  {
+    slug: 'brand_8',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-8.png'
+  },
+  {
+    slug: 'brand_9',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-9.png'
+  },
+  {
+    slug: 'brand_10',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-10.png'
+  },
+  {
+    slug: 'brand_11',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-11.png'
+  },
+  {
+    slug: 'brand_12',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-12.png'
+  },
+  {
+    slug: 'brand_13',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-13.png'
+  },
+  {
+    slug: 'brand_14',
+    icon_url: 'https://www.easypages.url.tw/w3layouts020/assets/images/brand-14.png'
+  }
+]
+
+export default brands
