72319533637d9f73dde50a9abcadba16eb131520
Duongbaxinh <duongbaxinh2021@gmail.com>
2024-06-22 10:25:04 +0700

    feat: create wishlist product card

--- src/components/molecules/WishlistProductCard/WishlistProductCard.vue
+++ src/components/molecules/WishlistProductCard/WishlistProductCard.vue
@@ -35,18 +35,12 @@
           content="Add To Cart"
           prefix-icon="fa-solid fa-cart-shopping "
         />
-        <button class="text-gray-500 hover:text-primary transition" @click="props.onDeleteToWishlist">
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16">
-            <g fill="currentColor">
-              <path
-                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"
-              ></path>
-              <path
-                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"
-              ></path>
-            </g>
-          </svg>
-        </button>
+        <RButton
+          class="border-none text-gray-500 hover:text-primary"
+          :onClick="onDeleteToWishlist"
+          variant="icon"
+          prefix-icon="fa-solid fa-trash"
+        />
       </div>
     </div>
   </div>
